8fc81ffab5cca720c553c551c43faae20a72c609
Garrett Girod <girodg@hotmail.com>
2025-01-15 12:03:39 -0500

    day 4,5

--- Week 2/Day04/Day04/Day04.cpp
+++ Week 2/Day04/Day04/Day04.cpp
@@ -11,145 +11,202 @@
 
 std::string toUpper(const std::string& original)
 {
-    std::string copy = original;
-    for (auto& c : copy)
-        c = toupper(c);
-    return copy;
+	std::string copy = original;
+	for (auto& c : copy)
+		c = toupper(c);
+	return copy;
 }
 
 void recursiveLoop(int N)
 {
-    recursiveLoop(N + 1);
+	if (N > 50) return;//exit condition
+	std::cout << N << "\n";//going out loop
+	recursiveLoop(N + 1);
+	std::cout << N << "\n";//coming back loop. unwinding the call stack.
 }
 
 unsigned long factorial(unsigned int N)
 {
-    if (N <= 1) return 1; //here's the exit condition!
-    return N * factorial(N - 1);
+	if (N <= 1) return 1; //here's the exit condition!
+	return N * factorial(N - 1);
 }
 
 /*
-    ╔═══════╗
-    ║Sorting║
-    ╚═══════╝
+	╔═══════╗
+	║Sorting║
+	╚═══════╝
 
-    Sorting is used to order the items in a vector/array is a specific way
+	Sorting is used to order the items in a vector/array is a specific way
 
-    CHALLENGE 2:
+	CHALLENGE 2:
 
-        Convert this BubbleSort pseudo-code into a method
+		Convert this BubbleSort pseudo-code into a method
 
-        procedure bubbleSort(A : list of sortable items)
-          n := length(A)
-          repeat
-              swapped := false
-              for i := 1 to n - 1 inclusive do
-                  if A[i - 1] > A[i] then
-                      swap(A, i - 1, i)
-                      swapped = true
-                  end if
-              end for
-              n := n - 1
-          while swapped
-        end procedure
-
+		procedure bubbleSort(A : list of sortable items)
+		  n := length(A)
+		  repeat
+			  swapped := false
+			  for i := 1 to n - 1 inclusive do
+				  if A[i - 1] > A[i] then
+					  swap(A, i - 1, i)
+					  swapped := true
+				  end if
+			  end for
+			  n := n - 1
+		  while swapped
+		end procedure
 */
+void bubbleSort(std::vector<std::string>& A)
+{
+	int n = A.size();
+	bool swapped = false;
+	do
+	{
+		swapped = false;
+		for (int i = 1; i <= n - 1; i++)
+		{
+			//_stricmp: < 0 less than, = 0 equal, > 0 greater than
+			if (_stricmp(A[i - 1].c_str(), A[i].c_str()) > 0)
+			{
+				//swap(A, i - 1, i); //TODO: FIX THIS!!!!
+				//std::string temp = A[i - 1];
+				//A[i - 1] = A[i];
+				//A[i] = temp;
+				//OR...
+				std::swap(A[i - 1], A[i]);
+
+				swapped = true;
+			}//end if
+		}//end for
+		n = n - 1;// n-=1; n--; --n;
+	} while (swapped);
+}//end procedure
+
+void FizzBuzz(int upperEnd)
+{
+	for (int number = 1; number < upperEnd; number++)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			std::cout << "FizzBuzz";
+		else if (number % 5 == 0)
+			std::cout << "Buzz";
+		else if (number % 3 == 0)
+			std::cout << "Fizz";
+		else
+			std::cout << number;
+		std::cout << "\n";
+	}
+}
+
+void Bats(int i=0)
+{
+	if (i < 100)
+	{
+		std::cout << (char)78 << (char)65 << ' ';
+		Bats(i+1);//++i pre-increment
+	}
+}
 
 int main()
 {
-    std::vector<std::string> names = { "Wonder Woman", "Superman", "Batman", "Flash", "Aquaman" };
-    //call your BubbleSort on the names vector.
+	FizzBuzz(100);
+	std::cin.get();
 
-    Console::WriteLine("--UNSORTED--", ConsoleColor::Yellow);
-    //print the sorted vector.
-    for (auto& name : names)
-        std::cout << name << "\n";
+	std::vector<std::string> names = { "Wonder Woman", "superman", "Batman", "Flash", "Aquaman" };
+	//call your BubbleSort on the names vector.
 
-    //call BubbleSort
+	Console::WriteLine("--UNSORTED--", ConsoleColor::Yellow);
+	//print the sorted vector.
+	for (auto& name : names)
+		std::cout << name << "\n";
 
-    Console::WriteLine("--SORTED--", ConsoleColor::Yellow);
-    //print the sorted vector.
-    for (auto& name : names)
-        std::cout << name << "\n";
+	//call BubbleSort
+	bubbleSort(names);
 
+	Console::WriteLine("--SORTED--", ConsoleColor::Yellow);
+	//print the sorted vector.
+	for (auto& name : names)
+		std::cout << name << "\n";
 
-    /*
-        ╔═════════╗
-        ║Recursion║
-        ╚═════════╝
+	std::cin.get();
 
-        Recursion happens when a method calls itself. This creates a recursive loop.
+	/*
+		╔═════════╗
+		║Recursion║
+		╚═════════╝
 
-        All recursive methods need an exit condition, something that prevents the loop from continuing.
+		Recursion happens when a method calls itself. This creates a recursive loop.
 
-    */
-    int N = 0;
-    recursiveLoop(N);
+		All recursive methods need an exit condition, something that prevents the loop from continuing.
 
-
-    /*
-        CHALLENGE 1:
-
-            convert this for loop to a recursive method called Bats.
-            Call Bats here in Main.
-
-            for(int i = 0;i < 100;i++)
-            {
-                std::cout << (char)78 << (char)65 << ' ';
-            }
-    */
-    //call Bats here.
-
-    char c[] = { '\n', 66, 65, 84, 77, 65, 78, 33, 33 };
-    for (auto ch : c) std::cout << ch;
+	*/
+	int N = 0;
+	recursiveLoop(N);
 
 
+	/*
+		CHALLENGE 1:
 
-    /*
-        ╔═════════════════╗
-        ║  Swapping Items ║
-        ╚═════════════════╝
+			convert this for loop to a recursive method called Bats.
+			Call Bats here in Main.
 
-        add #include <string_view>
-        make sure the project is using C++17 or greater.
+			for(int i = 0;i < 100;i++)
+			{
+				std::cout << (char)78 << (char)65 << ' ';
+			}
+	*/
+	//call Bats here.
+	Bats();
+	char c[] = { '\n', 66, 65, 84, 77, 65, 78, 33, 33 };
+	for (auto ch : c) std::cout << ch;
 
-    */
-    std::cout << "\n\n---SWAPPING ITEMS---\n";
-    std::vector<int> nums = { 1,2,3,4,5 };
-    for (auto i : nums)
-        std::cout << i << " ";
-    std::cout << " (original)\n";
 
-    //write code to swap 2 items in the vector
 
-    std::cout << " (after swapping)\n";
-    for (auto i : nums)
-        std::cout << i << " ";
+	/*
+		╔═════════════════╗
+		║  Swapping Items ║
+		╚═════════════════╝
 
-    /*
-        ╔═════════════════╗
-        ║Comparing Strings║
-        ╚═════════════════╝
+		add #include <string_view>
+		make sure the project is using C++17 or greater.
 
-        the compare functions return an int that will tell you 
-        how the strings compare to each other.
-        < 0 means s1 is LESS THAN (or comes before) s2
-          0 means s1 is EQUAL TO s2
-        > 0 means s1 is GREATER THAN (or comes after) s2
+	*/
+	std::cout << "\n\n---SWAPPING ITEMS---\n";
+	std::vector<int> nums = { 1,2,3,4,5 };
+	for (auto i : nums)
+		std::cout << i << " ";
+	std::cout << " (original)\n";
 
-        _stricmp: case-insensitive string comparison. 
-            The methods needs pointers to char arrays therefore 
-            you need to call c_str on the std::string
+	//write code to swap 2 items in the vector
 
-        std::string::compare:  case-sensitive string comparison
-            If you want to ignore case, you can convert the strings 
-            to all uppercase/lowercase
+	std::cout << " (after swapping)\n";
+	for (auto i : nums)
+		std::cout << i << " ";
 
-    */
-    std::string s1 = "Batman", s2 = "Aquaman";
-    int compResult = _stricmp(s1.c_str(), s2.c_str());
-    //OR...
-    int compareResult = toUpper(s1).compare(toUpper(s2));
+	/*
+		╔═════════════════╗
+		║Comparing Strings║
+		╚═════════════════╝
+
+		the compare functions return an int that will tell you
+		how the strings compare to each other.
+		< 0 means s1 is LESS THAN (or comes before) s2
+		  0 means s1 is EQUAL TO s2
+		> 0 means s1 is GREATER THAN (or comes after) s2
+
+		_stricmp: case-insensitive string comparison.
+			The methods needs pointers to char arrays therefore
+			you need to call c_str on the std::string
+
+		std::string::compare:  case-sensitive string comparison
+			If you want to ignore case, you can convert the strings
+			to all uppercase/lowercase
+
+	*/
+	std::string s1 = "Batman", s2 = "Aquaman";
+	int compResult = _stricmp(s1.c_str(), s2.c_str());
+	//OR...
+	int compareResult = toUpper(s1).compare(toUpper(s2));
 
 
 
--- Week 2/Day05/Day05/Day05.cpp
+++ Week 2/Day05/Day05/Day05.cpp
@@ -7,15 +7,46 @@
 #include <vector>
 #include "Console.h"
 #include "Input.h"
+#include <iomanip>
 
 enum class Weapon
 {
     Sword, Axe, Spear, Mace
 };
 
+/// <summary>
+/// Will search the vector using a linear search algorithm.
+/// </summary>
+/// <param name="numbers">the vector to search</param>
+/// <param name="number">the number to search for</param>
+/// <returns>-1 if not found, the index if found</returns>
+int LinearSearch(const std::vector<int>& numbers, int number)
+{
+    for (int i = 0; i < numbers.size(); i++)
+    {
+        if (numbers[i] == number)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
+
+void PrintGrades(const std::map<std::string, double>& course)
+{
+    Console::WriteLine("\nPG2 2501", ConsoleColor::Green);
+    for (auto& [student,grade] : course)
+    {
+        std::cout << std::setw(15) << std::left << student << " ";
+        std::cout << std::setw(7) << std::right;
+        Console::WriteLine(grade, ConsoleColor::White, 
+            ((grade < 59.5) ? ConsoleColor::Red : ConsoleColor::Black));
+    }
+}
 
 int main()
 {
+    srand(time(NULL));
     /*
         ╔═════════╗
         ║Searching║
@@ -37,8 +68,12 @@ int main()
 
     */
     std::vector<int> numbers = { 0,1,2,3,4,5,6 };
-    int searchNumber = 15;
-
+    int searchNumber = 6;
+    int foundIndex = LinearSearch(numbers, searchNumber);
+    if (foundIndex == -1)
+        std::cout << searchNumber << " was not found.\n";
+    else
+        std::cout << searchNumber << " was found at index " << foundIndex << "\n";
 
 
     /*
@@ -63,6 +98,22 @@ int main()
         1) using the insert method.
         2) using [key] = value
     */
+    std::map<std::string, float> menu;
+    //easy way. use map[key] = value;
+    menu["hot dog"] = 1.50f;
+    menu["ice cream"] = 2.99f;
+    menu["ice cream"] = 6.99f;//OVERWRITES any existing value
+
+    //"hard way". insert(pair)
+    std::pair<std::string, float> item = std::make_pair("chicken-fried steak", 13.99f);
+    auto wasInserted = menu.insert(item);//will NOT overwrite if the key already exists
+    //pair: first (iterator to the item in the map), second (bool: true if inserted)
+    if (wasInserted.second)
+        std::cout << "Menu item was inserted.\n";
+    else
+        std::cout << "Item was already on the menu. Overwrite?\n";
+
+
     std::map<Weapon, int> dorasBackpack;//will store the counts of each kind of weapon
 
     //returns an iterator and a bool. 
@@ -85,6 +136,19 @@ int main()
 
     */
 
+    std::vector<std::string> names{
+        "Juan","Nicolas","Ignacio","Kathryn","Joshua","Sherard","Brandon","Jacob","Ja'Sohn",
+"Daniel","Hunter","Michael","Aiden","Vy","Valeria","Matthew M","Xavier","Giancarlo","Abigail",
+"Sahaja","Dawson","Matthew T","Carlos","Griffin","Matthew R","Dominic","Nathan","Anna",
+"Charlotte","Imani","Justin","Maxwell","Quinton","Robert","Steven","Victor","William"
+    };
+    std::map<std::string, double> grades;
+    for (auto& name : names)
+    {
+        grades[name] = rand() % 10001 / 100.0;
+    }
+    PrintGrades(grades);
+
 
 
 
@@ -100,6 +164,44 @@ int main()
         You should use a ranged-based for loop when needing to loop over the entire map.
 
     */
+    std::cout << "\n\nPG2 Cafe\n";
+    for (auto& [menuItem,menuPrice] : menu)
+    {
+        std::cout << menuItem << " " << menuPrice << "\n";
+    }
+    for (auto i = menu.begin(); i != menu.end(); i++)
+    {
+        // *i to access the pair object
+        // OR...
+        // i-> to access the members of the pair object
+        std::string menuItem = i->first;// (*i).first;
+        float menuPrice = i->second; // (*i).second;
+    }
+
+    //iterator = find(key)
+    //not found is map.end()
+    std::string itemToFind = "hot dog";
+    float myItem = menu["Cheese Burger"];//BAD way to find a key
+    auto isFound = menu.find(itemToFind);
+    std::cout << "\n\nLooking for " << itemToFind << "...";
+    if (isFound == menu.end())
+    {
+        std::cout << itemToFind << " was not on the menu. Try McDonald's!\n";
+    }
+    else
+    {
+        float oldPrice = isFound->second;
+        isFound->second *= 1.10;
+        std::cout << isFound->first << " used to costs " << oldPrice << ".\n";
+        std::cout << "Now it costs " << isFound->second << "!! Thanks Putin!\n";
+    }
+
+    //HOW to update a value
+    //[key] = value will overwrite  (OK)
+    //iterator->second = value to update the value (BEST)
+
+
+
     for (auto const& [key, val] : dorasBackpack) //requires C++ 20
     {
         switch (key)
@@ -164,8 +266,28 @@ int main()
             If the student is found, print out the student's grade
             else print out a message that the student was not found
 
+            curve the grade (add 5) 
+                that is stored in the grades map
     */
+    do
+    {
+        std::cout << "\nStudent to curve: ";
+        std::string student;
+        std::getline(std::cin, student);
+        if (student.empty()) break;
 
+        auto foundStudent = grades.find(student);
+        if (foundStudent == grades.end())
+            std::cout << student << " was not found.\n";
+        else
+        {
+            std::cout << student << "'s grade was " << foundStudent->second << ".";
+            foundStudent->second = std::min(100.0, foundStudent->second + 5);
+            std::cout << " Now the grade is " << foundStudent->second << "\n";
+            PrintGrades(grades);
+        }
+
+    } while (true);
 
 
 
